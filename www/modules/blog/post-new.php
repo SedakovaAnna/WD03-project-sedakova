268763c350ffb853a25373735d809bbc30acc0c9
Anna Sedakova <tuzik198@mail.ru>
2018-07-23 14:01:47 +0300

    blog new post

--- www/modules/blog/post-new.php
+++ www/modules/blog/post-new.php
@@ -2,6 +2,93 @@
 
 $title = "Добавить новый пост";
 
+if ( isset($_POST['postNew'])) {
+
+	if ( trim($_POST['postTitle']) == '') {
+		$errors[] = ['title' => 'Введите Название поста' ];
+	}
+
+	if ( trim($_POST['postText']) == '') {
+		$errors[] = ['text' => 'Введите Текст поста' ];
+	}
+
+	if ( empty($errors)) {
+		$post = R::dispense('posts'); //создать
+		$post->title = htmlentities($_POST['postTitle']);
+		$post->cat = htmlentities($_POST['postCat']);
+		$post->dateTime = R::isoDateTime();
+		$post->text = $_POST['postText'];
+		$post->authorId = $_SESSION['logged_user']['id'];
+
+		if ( isset($_FILES["postImg"]["name"]) && $_FILES["postImg"]["tmp_name"] != "" ) {
+			
+			// Переменные, информация об изображении
+			$fileName = $_FILES["postImg"]["name"];
+			$fileTmpLoc = $_FILES["postImg"]["tmp_name"];
+			$fileType = $_FILES["postImg"]["type"];
+			$fileSize = $_FILES["postImg"]["size"];
+			$fileErrorMsg = $_FILES["postImg"]["error"];
+			$kaboom = explode(".", $fileName);
+			$fileExt = end($kaboom);
+
+			// Проверка изображения
+			list($width, $height) = getimagesize($fileTmpLoc);
+			if($width < 10 || $height < 10){
+				$errors[] = ['title' => 'Изображение не имеет размеров. Загрузите изображение побольше.' ];
+			}
+
+			if ( $fileSize > 4194304 ) {
+				$errors[] = ['title' => 'Файл изображения не должен быть более 4 Mb' ];
+			}
+
+			if ( !preg_match("/\.(gif|jpg|jpeg|png)$/i", $fileName) ) {
+				$errors[]  = [ 'title' => 'Неверный формат файла', 'desc' => '<p>Файл изображения должен быть в формате gif, jpg, jpeg, или png.</p>', ];
+			}
+
+			if ( $fileErrorMsg == 1 ) {
+				$errors[] = ['title' => 'При загрузке изображения произошла ошибка. Повторите попытку' ];
+			}
+
+			// Перемещаем загруженный файл в нужную директорию
+			$db_file_name = rand(100000000000,999999999999) . "." . $fileExt;
+			$postImgFolderLocation = ROOT . 'usercontent/blog/';
+			$uploadfile = $postImgFolderLocation . $db_file_name;
+			$moveResult = move_uploaded_file($fileTmpLoc, $uploadfile);
+			
+			if ($moveResult != true) {
+				$errors[] = ['title' => 'Ошибка сохранения файла' ];
+			}
+
+			include_once( ROOT . "/libs/image_resize_imagick.php");
+			
+			$target_file =  $postImgFolderLocation . $db_file_name;
+			$wmax = 920;
+			$hmax = 620;
+			$img = createThumbnailBig($target_file, $wmax, $hmax);
+			$img->writeImage($target_file);
+			$post->postImg = $db_file_name;
+
+			$target_file =  $postImgFolderLocation . $db_file_name;
+			$resized_file = $postImgFolderLocation . "320-" . $db_file_name;
+			$wmax = 320;
+			$hmax = 140;
+			$img = createThumbnailCrop($target_file, $wmax, $hmax);
+			$img->writeImage($resized_file);
+			$post->postImgSmall = "320-" . $db_file_name;
+
+		}
+
+		R::store($post);
+		header('Location: ' . HOST . "blog");
+			// ?result=postCreated");
+		exit();
+
+
+	}
+
+
+
+}
 // Готовим контент для центральной части
 ob_start();
 include ROOT . "templates/_parts/_header.tpl";
